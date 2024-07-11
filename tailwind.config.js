223b4b20a889dac267ed3cd828e62be5ffe577bf
vcpersike <vcpersike@gmail.com>
2024-07-11 20:25:19 -0300

    Primeiro Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content:[
+    '.index.html',
+    './src/**/*.{html,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'microFront03',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Menu': './src/Menu',
+      },
+      shared: ['react', 'react-dom'],
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    port: 3003
+  },
+  preview: {
+    port: 3003
+  }
+})
